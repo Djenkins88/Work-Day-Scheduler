4e2ab7ec87386a3645db186da5edab4538b3e7e4
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-06-11 15:27:26 -0500

    added save in local storage and get from local storage

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 $(".saveBtn").on("click", function() {
-    // geting text and row values
+    // getting text and row values
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    // save in localStorage
-    localStorage.setItem(time /* aka KEY = row id */, value /* text in the textarea */);
+    // save in local storage
+    localStorage.setItem(time,value);
 });
+// get from local storage
+$("9.description").val(localStorage.getItem("9")); 
+$("10.description").val(localStorage.getItem("10")); 
+$("11.description").val(localStorage.getItem("11")); 
+$("12.description").val(localStorage.getItem("12"));
+$("1.description").val(localStorage.getItem("1"));
+$("2.description").val(localStorage.getItem("2"));
+$("3.description").val(localStorage.getItem("3"));
+$("4.description").val(localStorage.getItem("4"));
+$("5.description").val(localStorage.getItem("5"));
 
 
