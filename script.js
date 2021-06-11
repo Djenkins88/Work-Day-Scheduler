1f497a15d6d102c7826471b76008b2aac38a5749
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-06-11 17:40:28 -0500

    added function for past present future to js file

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(".saveBtn").on("click", function() {
     // save in local storage
     localStorage.setItem(time,value);
 });
+
 // get from local storage
 $("9.description").val(localStorage.getItem("9")); 
 $("10.description").val(localStorage.getItem("10")); 
@@ -20,4 +21,31 @@ $("3.description").val(localStorage.getItem("3"));
 $("4.description").val(localStorage.getItem("4"));
 $("5.description").val(localStorage.getItem("5"));
 
+function updateHour() {
+    // get current number of hours
+    var currentHour = moment().hours();
 
+    // loop over time blocks
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  updateHour();
+
+  // set up interval to check if current time needs to be updated
+  //var interval = setInterval(updateHour, 15000);
