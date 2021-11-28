59197170e36129bff41bc7e4499c1750425500dc
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-11-27 21:44:22 -0600

    test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Work-Day-Scheduler
 
-This was 
+![](images/img1.png)
+
+This was a project making a work day schedular using html, css, javascript, Qjuery and Bootstrap.
--- script.js
+++ script.js
@@ -2,40 +2,42 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY,"))
 
 
-
 var currentTime = moment().format("LT");
 $("#currentTime").append(currentTime);
 
 // function for saving inputs to local storage
 $(".saveBtn").on("click", function() {
     
-    var value = $(this).siblings(".description").val();
+    var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     
-    localStorage.setItem(time,value);
+    localStorage.setItem(time,text);
 });
 
 // getting from local storage
+
 $("9.description").val(localStorage.getItem("9")); 
 $("10.description").val(localStorage.getItem("10")); 
 $("11.description").val(localStorage.getItem("11")); 
 $("12.description").val(localStorage.getItem("12"));
-$("1.description").val(localStorage.getItem("1"));
-$("2.description").val(localStorage.getItem("2"));
-$("3.description").val(localStorage.getItem("3"));
-$("4.description").val(localStorage.getItem("4"));
-$("5.description").val(localStorage.getItem("5"));
+$("13.description").val(localStorage.getItem("13"));
+$("14.description").val(localStorage.getItem("14"));
+$("15.description").val(localStorage.getItem("15"));
+$("16.description").val(localStorage.getItem("16"));
+$("17.description").val(localStorage.getItem("17"));
 
 // function to track current hour and show that hour by showing different colored time blocks
 function updateHour() {
   
-    
     var currentHour = moment().hours();
 
     // loop over time blocks
     $(".time-block").each(function() {
-      var blockHour = parseInt($(this).attr("id"));
+      var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+      //split("hour")[1]);
+
+     // var blockHour = parseInt($(this).attr("id"));
 
       // checking times for color
       if (blockHour < currentHour) {
